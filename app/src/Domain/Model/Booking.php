7f7d9cfbc4e45a8556acb77c0a0daf091714b498
Albert Ferraté <aferrate.afm@gmail.com>
2022-06-08 21:40:25 +0200

    update booking model

--- app/src/Domain/Model/Booking.php
+++ app/src/Domain/Model/Booking.php
@@ -66,31 +66,4 @@ final class Booking
     {
         $this->date = $date;
     }
-
-    /**
-     * @return array
-     */
-    public function returnArrayBooking(Booking $booking): array
-    {
-        $data[] = [
-            'id' => $booking->getId(),
-            'memberId' => $booking->getIdMember(),
-            'classroomId' => $booking->getIdClassroom(),
-            'date' => $booking->getDate()
-        ];
-
-        return $data;
-    }
-
-    /**
-     * @return Booking
-     */
-    public static function returnObjBooking(Booking $booking, array $bookingArr): Booking
-    {
-        $booking->setIdMember($bookingArr['member_id']);
-        $booking->setIdClassroom($bookingArr['classroom_id']);
-        $booking->setDate(DateTime::createFromFormat('d-m-Y', $bookingArr['date']));
-
-        return $booking;
-    }
 }
